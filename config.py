79a25daed5dfc41f6db60a146ac53ce36d33a78e
OkothPius <orukopius8@gmail.com>
2019-04-29 17:53:22 +0300

    add installations

--- config.py
+++ config.py
@@ -7,7 +7,7 @@ class Config:
     SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://orukopius:blog@localhost/blog'
     SECRET_KEY = 'oruko@jkl'
     UPLOADED_PHOTOS_DEST = 'app/static/photos'
-    MAIL_SERVER = 'smtp.gmail.com'
+    MAIL_SERVER = 'smtp.googlemail.com'
     MAIL_PORT = 587
     MAIL_USE_TLS = True
     MAIL_USERNAME = os.environ.get("MAIL_USERNAME")
