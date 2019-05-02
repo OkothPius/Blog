bba658a5899636c3124ca5eb5bc17293acaa5713
OkothPius <orukopius8@gmail.com>
2019-05-02 12:14:47 +0300

    adding tests

--- manage.py
+++ manage.py
@@ -5,6 +5,7 @@ from app.models import User, Role, Post
 
 # Creating app instance
 app = create_app('development')
+app = create_app('test')
 
 manager = Manager(app)
 manager.add_command('server', Server)
